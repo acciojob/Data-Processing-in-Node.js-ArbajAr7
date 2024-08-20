59df47bd43e15199f97d6c39ab2b55323c8b84e5
ArbajAr7 <arbajrmacr786@gmail.com>
2024-08-21 00:33:34 +0530

    first commit

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
-Implement the processData function in app.js:
-
 const fs = require('fs');
 const { Transform } = require('stream');
 
 function processData(inputFilePath, outputFilePath) {
-  // Implement this function
+  const fileStream = fs.createReadStream(inputFilePath);
+  const transformedData = fs.createWriteStream(outputFilePath);
+
+  const uppercase = new Transform({
+    transform(chunk, encoding, callback) {
+      callback(null, chunk.toString().toUpperCase());
+    },
+  });
+
+  fileStream.pipe(uppercase).pipe(transformedData);
 }
 
 processData('input.txt', 'output.txt');
-
